d94c00889a58ec7904c44e5e3d04a2bcd74a28e3
Yussefgafer <145143218+Yussefgafer@users.noreply.github.com>
2025-09-01 21:39:31 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
 const cacheName = 'my-counter-app-v1';
 const assetsToCache = [
-  '/',
-  '/index.html' 
+  '/TiK/',         <!-- تم التعديل هنا (أضف اسم المستودع) -->
+  '/TiK/index.html' <!-- تم التعديل هنا (أضف اسم المستودع) -->
+  // ملاحظة: بما أن ملفات CSS و JS داخل ملف HTML، لا نحتاج لإضافتها هنا.
 ];
 
+// Install Event: يتم تخزين الملفات هنا
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName)
@@ -13,7 +15,7 @@ self.addEventListener('install', e => {
   );
 });
 
-
+// Activate Event: يتم هنا تنظيف الكاش القديم إذا تغير اسم الكاش
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(cacheNames => {
@@ -28,11 +30,12 @@ self.addEventListener('activate', e => {
   );
 });
 
-
+// Fetch Event: يتم تقديم الملفات من الكاش إذا كانت متاحة
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request)
       .then(response => {
+        // إذا وجدنا الملف في الكاش، نعيده، وإلا نطلبه من الشبكة
         return response || fetch(e.request);
       })
   );
